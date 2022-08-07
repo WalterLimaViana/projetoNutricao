9569d2c3b3c182e324154831252fdb269798b5f6
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-06 22:44:44 -0300

    Replicar o código para os demais pacientes

--- js/main.js
+++ js/main.js
@@ -2,26 +2,31 @@ var title = document.querySelector('.title');
 title.textContent = "Nutrição";
 //Cálculo IMC
 //pegar dados para calcular o IMC
-var paciente = document.querySelector('#first');
-console.log(paciente);
-var tdPeso = paciente.querySelector('.info-peso');
-var peso = tdPeso.textContent;
-var tdAltura = paciente.querySelector('.info-altura');
-var altura = tdAltura.textContent;
-console.log(peso);
-console.log(altura);
-//Pegando o valor do IMC na tabela para substituir pelo cálculo:
-var tdImc = paciente.querySelector('.info-imc');
-console.log(tdImc);
+var pacientes = document.querySelectorAll('.paciente');
 
-if (peso <= 0 || peso >= 400) {
-    alert('Peso Inválido!');
-} else if (altura <= 0 || altura >= 3.00) {
-    alert('Altura Inválida!');
-} else {
-    // Calculando o IMC
-    var imc = peso / (altura * altura);
-    //Colocar o resultado do IMC dentro da tabela no html
-    tdImc.textContent = imc;
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+    console.log(peso);
+    console.log(altura);
+    //Pegando o valor do IMC na tabela para substituir pelo cálculo:
+    var tdImc = paciente.querySelector('.info-imc');
+    console.log(tdImc);
+
+    if (peso <= 0 || peso >= 400) {
+        alert('Peso Inválido!');
+    } else if (altura <= 0 || altura >= 3.00) {
+        alert('Altura Inválida!');
+    } else {
+        // Calculando o IMC
+        var imc = peso / (altura * altura);
+        //Colocar o resultado do IMC dentro da tabela no html
+        tdImc.textContent = imc.toFixed(0);
+    }
 }
+
 
