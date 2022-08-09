1b21aa5f684ff717765e7b530260e434b94a9226
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-08 22:48:21 -0300

    criando setTimeout para melhorar a transição

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,24 +1,22 @@
-
 // Seleciona a tabela inteira
-var tabela = document.querySelectorAll('table');
-//Para cada item da tabela, adiciona um listener
-tabela.forEach(function (tabela) {
-    tabela.addEventListener('dblclick', function (event) {
-        //Colocando um efeito para excluir a linha utilizando um fadeOut
-        event.target.parentNode.classList.add('fadeOut');
+var tabela = document.querySelector("#tabela-pacientes");
+tabela.addEventListener("dblclick", function (event) {
+    //Colocando um efeito para excluir a linha utilizando um fadeOut
+    event.target.parentNode.classList.add("fadeOut");
 
-        //Criar uma variável do item a ser clicado
-        // var eventTarget = event.target;
-        /*Criar uma variável do pai do item, pois assim ao invés de excluir somente um item da tabela 
-        será excluido a linha inteira, que é o pai(tr).*/
-        // var targetFather = event.target.parentNode;
-        //Removendo o pai
-        // targetFather.remove();
+    //Criar uma variável do item a ser clicado
+    // var eventTarget = event.target;
+    /*Criar uma variável do pai do item, pois assim ao invés de excluir somente um item da tabela 
+    será excluido a linha inteira, que é o pai(tr).*/
+    // var targetFather = event.target.parentNode;
+    //Removendo o pai
+    // targetFather.remove();
 
-        //Ou de forma mais simples: 
-
+    //Ou de forma mais simples: 
+    //Utilizandoo setTimeOut para que a função remove aguarde a inserção do fadeOut executar antes de remover
+    setTimeout(function () {
         event.target.parentNode.remove();
-    });
+    }, 500);
 });
 // var pacientes = document.querySelectorAll('.paciente');
 // pacientes.forEach(function (paciente) {
