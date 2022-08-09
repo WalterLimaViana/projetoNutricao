fc9e47b5a22a94631912e22eeb33385d8b9da76c
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-08 22:31:14 -0300

    criando um efeito para a transição da exclusão

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,21 +1,28 @@
-// var pacientes = document.querySelectorAll('.paciente');
-// pacientes.forEach(function (paciente) {
-//     paciente.addEventListener('dblclick', function () {
-//         this.remove();
-//     });
-// });
 
 // Seleciona a tabela inteira
 var tabela = document.querySelectorAll('table');
 //Para cada item da tabela, adiciona um listener
 tabela.forEach(function (tabela) {
     tabela.addEventListener('dblclick', function (event) {
-        //Criar uma variável do item a ser cliclado
-        var eventTarget = event.target;
+        //Colocando um efeito para excluir a linha utilizando um fadeOut
+        event.target.parentNode.classList.add('fadeOut');
+
+        //Criar uma variável do item a ser clicado
+        // var eventTarget = event.target;
         /*Criar uma variável do pai do item, pois assim ao invés de excluir somente um item da tabela 
         será excluido a linha inteira, que é o pai(tr).*/
-        var targetFather = event.target.parentNode;
+        // var targetFather = event.target.parentNode;
         //Removendo o pai
-        targetFather.remove();
+        // targetFather.remove();
+
+        //Ou de forma mais simples: 
+
+        event.target.parentNode.remove();
     });
 });
+// var pacientes = document.querySelectorAll('.paciente');
+// pacientes.forEach(function (paciente) {
+//     paciente.addEventListener('dblclick', function () {
+//         this.remove();
+//     });
+// });
